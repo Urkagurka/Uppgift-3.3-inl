04b96a9bc373c3696562ce81a7e22bfab231980e
alve06001 <alve06001@E-PW0234LA>
2023-10-06 13:27:08 +0200

    Add project files.

---
+++ Uppgift 3.3 inl/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace Uppgift_3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många timmar vill du hyra bilen?");
+            int timmar = int.Parse(Console.ReadLine());
+            int pris = timmar * 80;
+
+            if (pris > 950)
+            {
+                Console.WriteLine("Det kostar mer än 950kr");
+            }
+            else if (pris < 950)
+            {
+                Console.WriteLine("Det kostar " + pris + "kr");
+            }
+
+        }
+    }
+}
